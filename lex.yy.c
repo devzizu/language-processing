3692e5b4b74c6d8d55493cc9d249bc3d7f40ebb3
PedroFCM <pedrofcmachado26@gmai.com>
2020-03-13 16:29:47 +0000

    Update do filtro

--- lex.yy.c
+++ lex.yy.c
@@ -360,11 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[25] =
+static const flex_int16_t yy_accept[21] =
     {   0,
         0,    0,    4,    2,    2,    0,    0,    0,    0,    0,
-        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-        0,    0,    1,    0
+        0,    0,    0,    0,    0,    0,    0,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -372,24 +371,24 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    3,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    4,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        5,    1,    1,    1,    6,    1,    7,    8,    9,    1,
-        1,    1,   10,    1,    1,    1,   11,   12,   13,    1,
-        1,    1,    1,   14,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,   15,    1,   16,   17,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    5,    1,
 
-       18,    1,    1,    1,   19,    1,    1,    1,   20,   21,
-       22,    1,    1,    1,    1,   23,    1,    1,    1,    1,
+        6,    1,    1,    1,    7,    1,    1,    1,    8,    9,
+       10,    1,    1,   11,   12,   13,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+       14,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,   15,    1,    1,    1,    1,    1,
 
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,47 +398,42 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[24] =
+static const YY_CHAR yy_meta[16] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1
+        1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[27] =
+static const flex_int16_t yy_base[22] =
     {   0,
-        0,    1,   32,   38,    0,    0,    1,   27,    3,    0,
-        0,    1,   15,   13,   12,   26,   17,   20,   24,    2,
-        1,    0,   38,   38,    1,    0
+       22,   21,   24,   27,    0,   18,    2,   12,   13,   14,
+       10,    5,    2,    2,    4,    7,    3,    0,   27,   27,
+        0
     } ;
 
-static const flex_int16_t yy_def[27] =
+static const flex_int16_t yy_def[22] =
     {   0,
-       25,   25,   24,   24,   24,   24,   24,   24,   24,   24,
-       24,   24,   24,   24,   24,   24,   24,   24,   24,   24,
-       26,   26,   24,    0,   24,   24
+       21,   21,   20,   20,   20,   20,   20,   20,   20,   20,
+       20,   20,   20,   20,   20,   20,   20,   20,   20,    0,
+       20
     } ;
 
-static const flex_int16_t yy_nxt[62] =
+static const flex_int16_t yy_nxt[43] =
     {   0,
-       22,    4,   23,   23,   21,    6,    8,    5,    5,   10,
-       12,   13,   11,    7,    6,    8,    5,    5,   10,   12,
-       13,   11,    7,   14,   15,   16,   18,   19,   20,   17,
-        9,   24,   14,   15,   16,   18,   19,    3,   24,   24,
-       24,   24,   24,   24,   24,   24,   24,   24,   24,   24,
-       24,   24,   24,   24,   24,   24,   24,   24,   24,   24,
-       24
+        4,   20,    6,    7,    6,    7,   20,   20,   20,   20,
+       20,   19,   18,   17,   16,   15,   14,   13,   12,   11,
+       10,    9,    8,   20,    5,    5,    3,   20,   20,   20,
+       20,   20,   20,   20,   20,   20,   20,   20,   20,   20,
+       20,   20
     } ;
 
-static const flex_int16_t yy_chk[62] =
+static const flex_int16_t yy_chk[43] =
     {   0,
-       26,   25,   22,   21,   20,    5,    7,    1,    2,    9,
-       11,   12,   10,    6,    5,    7,    1,    2,    9,   11,
-       12,   10,    6,   13,   14,   15,   17,   18,   19,   16,
-        8,    3,   13,   14,   15,   17,   18,   24,   24,   24,
-       24,   24,   24,   24,   24,   24,   24,   24,   24,   24,
-       24,   24,   24,   24,   24,   24,   24,   24,   24,   24,
-       24
+       21,    0,    5,    5,    7,    7,    0,    0,    0,    0,
+        0,   18,   17,   16,   15,   14,   13,   12,   11,   10,
+        9,    8,    6,    3,    2,    1,   20,   20,   20,   20,
+       20,   20,   20,   20,   20,   20,   20,   20,   20,   20,
+       20,   20
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -456,32 +450,28 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "plfiltrobase.l"
-#line 2 "plfiltrobase.l"
+#line 1 "filtro.l"
+#line 2 "filtro.l"
 /* Declaracoes C diversas */
 
 int numberComments;
 
-struct commentThread 
+typedef struct commentThread 
 {
    char*  id;
-   char  user[50];
-   char  date[100];
+   char* user;
+   char*  date;
    int	timestamp;
-   char	commentText[200];
+   char	*commentText;
    int   likes;
    int   hasReplies;
    int   numberReplies;
 
-   struct commentThread* replies;
-};
+   // struct commentThread* replies;
+} COMMENT_T;
 
-typedef struct commentThread* commentT;
-
-char dest[50];
-
-#line 484 "lex.yy.c"
-#line 485 "lex.yy.c"
+#line 474 "lex.yy.c"
+#line 475 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -698,10 +688,10 @@ YY_DECL
 		}
 
 	{
-#line 26 "plfiltrobase.l"
+#line 22 "filtro.l"
 
 
-#line 705 "lex.yy.c"
+#line 695 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -728,13 +718,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 25 )
+				if ( yy_current_state >= 21 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 38 );
+		while ( yy_base[yy_current_state] != 27 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -759,26 +749,28 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
+*yy_cp = (yy_hold_char); /* undo effects of setting up yytext */
+(yy_c_buf_p) = yy_cp -= 13;
+YY_DO_BEFORE_ACTION; /* set up yytext again */
 YY_RULE_SETUP
-#line 28 "plfiltrobase.l"
+#line 24 "filtro.l"
 {
-                  ct->id = strncpy(dest, yytext + 18, strlen(yytext + 18) - 1);
-                  printf("%s\n", ct -> id);
-                  }
+                           numberComments = atoi(yytext);
+                           printf("Number of Comments: %d\n", numberComments);
+                        }
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 32 "plfiltrobase.l"
+#line 28 "filtro.l"
 
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 34 "plfiltrobase.l"
+#line 30 "filtro.l"
 ECHO;
 	YY_BREAK
-#line 782 "lex.yy.c"
+#line 774 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1075,7 +1067,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 25 )
+			if ( yy_current_state >= 21 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1103,11 +1095,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 25 )
+		if ( yy_current_state >= 21 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 24);
+	yy_is_jam = (yy_current_state == 20);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1783,7 +1775,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 34 "plfiltrobase.l"
+#line 30 "filtro.l"
 
 int yywrap()
 { 
@@ -1792,9 +1784,9 @@ int yywrap()
 
 
 int main()
-{ 
-   commentT ct = malloc(sizeof(struct commentThread));
-   yylex(); return 0; 
+{
+   
+  yylex(); return 0; 
 }
 
 
